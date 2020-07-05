806806813b2410c93c7364a58ea4ca4d8686f124
jonathancsr <jonathancsrr@gmail.com>
2020-07-05 16:09:04 -0300

    add: toast use

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/auth';
+import { ToastProvider } from './hooks/toast';
 
 import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
-      <SignIn />
-    </AuthProvider>
+    <ToastProvider>
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
+    </ToastProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
