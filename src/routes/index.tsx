7a26258432efedebfe99b640694bbde35b23c323
jonathancsr <jonathancsrr@gmail.com>
2020-07-05 21:24:48 -0300

    add: Dashboard page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} />
   </Switch>
 );
 
